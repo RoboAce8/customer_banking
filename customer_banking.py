c69d206db55bb6164542ca26d4b241ba43bd1b13
RoboAce8 <hunterklinglesmith@gmail.com>
2024-12-16 20:07:51 -0700

    Updated print statements in main function.

--- customer_banking.py
+++ customer_banking.py
@@ -17,7 +17,7 @@ def main():
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    print(f"After {savings_maturity} months, the account has earned ${interest_earned:,.2f}. The new balance is {updated_savings_balance:,.2f}")
+    print(f"After {savings_maturity} months, the account has earned ${interest_earned:,.2f}. The new balance is ${updated_savings_balance:,.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     cd_balance = float(input("Enter the CD Account Balance: "))
@@ -28,7 +28,7 @@ def main():
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
-    print(f"After {savings_maturity} months, the account has earned {interest_earned:,.2f}. The new balance is {updated_savings_balance:,.2f}")
+    print(f"After {savings_maturity} months, the account has earned ${interest_earned:,.2f}. The new balance is ${updated_savings_balance:,.2f}")
 
 if __name__ == "__main__":
     main()
